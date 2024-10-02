c7a5bbade228d697a8e8af56f43de051c2cd5246
Muhammad Nurul Karim <karimmitra0@gmail.com>
2024-10-03 01:57:17 +0700

    Feat: Update Gradle

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,10 +1,17 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-
     id("com.google.devtools.ksp")
     id("kotlin-parcelize")
+//    id("com.squareup.leakcanary.deobfuscation")
 }
+
+//leakCanary {
+//    // LeakCanary needs to know which variants have obfuscation turned on
+//    filterObfuscatedVariants { variant ->
+//        variant.name == "debug"
+//    }
+//}
 
 android {
     namespace = "com.mhmdnurulkarim.core"
@@ -15,7 +22,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_URL", "\"https://api.github.com\"")
-        buildConfigField("String", "API_KEY", "\"Isi Token Sendiri\"")
+        buildConfigField("String", "API_KEY", "\"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\"")
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -42,26 +49,27 @@ android {
 }
 
 dependencies {
-    api("androidx.core:core-ktx:1.12.0")
-    api("androidx.appcompat:appcompat:1.6.1")
-    api("com.google.android.material:material:1.11.0")
+    api("androidx.core:core-ktx:1.13.1")
+    api("androidx.appcompat:appcompat:1.7.0")
+    api("com.google.android.material:material:1.12.0")
     api("androidx.constraintlayout:constraintlayout:2.1.4")
+
     testApi("junit:junit:4.13.2")
-    androidTestApi("androidx.test.ext:junit:1.1.5")
-    androidTestApi("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestApi("androidx.test.ext:junit:1.2.1")
+    androidTestApi("androidx.test.espresso:espresso-core:3.6.1")
     api("androidx.multidex:multidex:2.0.1")
 
-    api("androidx.activity:activity-ktx:1.8.2")
-    api("androidx.fragment:fragment-ktx:1.6.2")
+    api("androidx.activity:activity-ktx:1.9.2")
+    api("androidx.fragment:fragment-ktx:1.8.4")
 
     //Coroutine
     api ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     api ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    api("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    api ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    api("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    api("androidx.navigation:navigation-ui-ktx:2.7.7")
+    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    api("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
+    api ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    api("androidx.navigation:navigation-fragment-ktx:2.8.2")
+    api("androidx.navigation:navigation-ui-ktx:2.8.2")
 
     //Injection: Koin
     api("io.insert-koin:koin-core:3.5.3")
@@ -79,9 +87,13 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
 
     //DataStore
-    api("androidx.datastore:datastore-preferences:1.0.0")
+    api("androidx.datastore:datastore-preferences:1.1.1")
 
     //Glide
     api("com.github.bumptech.glide:glide:4.16.0")
     api("de.hdodenhof:circleimageview:3.1.0")
+
+    //LeakCanary
+    debugApi("com.squareup.leakcanary:leakcanary-android:2.14")
+    api("com.squareup.leakcanary:leakcanary-deobfuscation-gradle-plugin:2.4")
 }
--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -35,9 +35,9 @@ android {
 dependencies {
     implementation(project(":core"))
     implementation(project(":app"))
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    androidTestImplementation("androidx.annotation:annotation:1.7.1")
+    androidTestImplementation("androidx.annotation:annotation:1.8.2")
 }
